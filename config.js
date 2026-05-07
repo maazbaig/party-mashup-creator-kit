e0d75d06ddda61bb859e5e163dcb3ff530081551
maazbaig <maazbaig117@gmail.com>
2026-05-07 18:23:37 -0500

    Configure Supabase creator request form

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PARTY_MASHUP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://uvrtpeaxnrpztdrdcpwn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_auAQOtC3r2txZpZ_E_vxbQ_JebBsyGU"
 };
